3089f45555504055903d5ff65b8c61214e6d4c00
NathanRodriguesVieira99 <nathanrodriguesvieira99@gmail.com>
2026-08-01 18:54:34 -0300

    feat: add router subpackage

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+/*
+* Em GO se importa o pacote completo.
+ */
+import "github.com/NathanRodriguesVieira99/API-Go/router"
+
+/*
+* := É definir e declarar uma variável.
+ */
+func main() {
+
+	router.Initialize()
+
+}
